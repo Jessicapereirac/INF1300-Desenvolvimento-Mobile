1fa10d5fe2d7109abf259d942a67df9207385734
Jéssica Pereira <jessicapereiraa2@gmail.com>
2020-05-03 17:44:02 -0300

    pag das profissionais

--- disciplina/at_home/lib/professional_list.dart
+++ disciplina/at_home/lib/professional_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class professional_List extends StatefulWidget {
@@ -13,14 +14,17 @@ class _professional_listState extends State<professional_List> {
   String _service;
   List<String> _name = ["Daniele Lima de Souza", "Ricardo Heffer Matheus","Gabriel de Souza Heffer Matheus","Paulo Vitor Dias Linhares"];
   List<double> _grade = [4.7, 4.4,5.0,4.1];
+  List<String> _imagens = ["med1.jpg", "med2.jpg", "med3.jpeg", "med1.jpg" ];
   _professionalListBiulder _professionalBiulder;
 
   _professional_listState(this._service) {
-    _professionalBiulder = new _professionalListBiulder(_name, _grade);
+    _professionalBiulder = new _professionalListBiulder(_name, _grade, _imagens);
   }
+
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.white,
@@ -55,15 +59,72 @@ class _professional_listState extends State<professional_List> {
 class _professionalListBiulder {
   List<String> name;
   List<double> grade;
+  List<String> imagens;
 
-  _professionalListBiulder(this.name, this.grade);
+  _professionalListBiulder(this.name, this.grade, this.imagens);
 
   @override
   Widget professional_builder(BuildContext context, int index) {
     double grade_index = this.grade[index];
     return ListTile(
         leading: CircleAvatar(
-          backgroundImage: AssetImage("images/gatineo.jpg"),
+          child: GestureDetector(
+            child: ClipOval(
+                child: Image.asset("images/"+ imagens[index],
+
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,)
+            ),
+            onTap: (){
+
+              showCupertinoModalPopup(context: context,
+                builder: (BuildContext context)  => CupertinoActionSheet (
+                    title: Text(this.name[index]),
+                    message: GestureDetector(
+                      child: Text('mais detalhes... ',
+
+                        style:
+                        TextStyle(
+                            color: Colors.lightBlueAccent,
+                            fontSize: 14,
+                            fontFamily: 'Roboto'
+                        ),
+                      ),
+                      onDoubleTap: (){
+                        print("http request");
+                      },
+                    ),
+                    actions: <Widget>[
+                      CupertinoActionSheetAction(
+                        child: const Text('Ligar'),
+                        onPressed: () {
+                          Navigator.pop(context, 'ligar');
+                        },
+                      ),
+                      CupertinoActionSheetAction(
+                        child: const Text('Mandar email'),
+                        onPressed: () {
+                          Navigator.pop(context, 'email');
+                        },
+                      )
+                    ],
+                    cancelButton: CupertinoActionSheetAction(
+                      child: const Text('Cancelar'),
+                      isDefaultAction: true,
+                      onPressed: () {
+                        Navigator.pop(context, 'Cancel');
+                      },
+                    )
+                ),
+
+              );
+
+            },
+          ),
+          radius: 30,
+
+
         ),
         title: Text(
           this.name[index],
@@ -82,8 +143,14 @@ class _professionalListBiulder {
               ),
             )
           ],
+
+
         ),
-        onTap: (){print("tap");},
+
         );
   }
 }
+
+
+
+
