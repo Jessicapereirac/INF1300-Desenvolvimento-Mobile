e8a44762d4e1c53ce6e53448a4c7b06d18b0db50
Jéssica Pereira <jessicapereiraa2@gmail.com>
2020-06-14 14:05:48 -0300

    em tese deveria ta funcionando

--- disciplina/at_home/lib/internacionalizacao/localizations.dart
+++ disciplina/at_home/lib/internacionalizacao/localizations.dart
@@ -9,50 +9,22 @@ class AppLocalizations {
 
   AppLocalizations(this.locale);
 
-  Map<String, dynamic> _localizationsStrings; // Mudança de String para Dynamic
+  Map<String, String> _localizationsStrings; // Mudança de String para Dynamic
 
   Future<bool> load() async {
-    var jsonString =
+    String jsonString =
     await rootBundle.loadString('lang/${locale.languageCode}.json');
     Map<String, dynamic> jsonMap =
     json.decode(jsonString); // Mudança de String para Dynamic
 
     _localizationsStrings = jsonMap.map((key, value) {
-      return MapEntry(key, value); // Remover toString
+      return MapEntry(key, value.toString()); // Remover toString
     });
     return true;
   }
 
-  String translate(String key,
-      {Map<String, String> params, String defaultValue = ''}) {
-    var value;
-    // Modificação do método para pegar json concatenado com . por nível
-
-    if (key.contains('.')) {
-      key.split('.').forEach((element) {
-        if (value == null) {
-          value = _localizationsStrings[element];
-        } else {
-          value = value[element];
-        }
-      });
-    }
-
-    // tratamento para caso não venha nada
-    if (value == null) {
-      return throw ArgumentError(
-          'key: $key not found in ${locale.languageCode}.json');
-    }
-
-    // parametros para poder concatenar valores com o texto
-    if (params != null) {
-      params.forEach((chave, valor) {
-        value = value.replaceAll('{{$chave}}', valor);
-      });
-    }
-
-    // caso valor esteja nulo retorna o valor default
-    return value ?? defaultValue;
+  String translate(String key) {
+    return _localizationsStrings[key];
   }
 
   static AppLocalizations of(BuildContext context) {
